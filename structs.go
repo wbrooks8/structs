1027178ad26886a8a6351a17ff6defaf7fa6900d
KyleBrooks <kyle.brooks1989@gmail.com>
2026-08-16 18:24:00 -0500

    First commit

---
+++ structs.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"example.com/pointers/user"
+)
+
+func main() {
+	var appUser *user.User
+	var err error
+
+	appUser, err = user.NewUser()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+}
---
+++ user/user.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName  string
+	lastName   string
+	birthdate  string
+	createdAtt time.Time
+}
+
+// Nests the function inside of user struct
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *User) ClearUserName () {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func NewUser() (*User, error) {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthDate := getUserData("Please enter your birthday (MM/DD/YYYY): ")
+
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("First name, last name, and birthdate are required.")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthDate,
+		createdAtt: time.Now(),
+	}, nil
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scanln(&value)
+	return value
+}
